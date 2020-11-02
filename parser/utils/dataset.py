6a7f044e5397406cfa8c98f5f9c3c0ecdece09a8
Alireza Mohammadshahi <alireza.mohammadshahi@epfl.ch>
2020-11-02 15:27:29 +0100

    Update dataset.py

--- parser/utils/dataset.py
+++ parser/utils/dataset.py
@@ -1,7 +1,21 @@
 
-"""
-A Dataset Reader for Universal Dependencies, with support for multiword tokens and special handling for NULL "_" tokens
-"""
+#Copyright (c) 2020 Idiap Research Institute, http://www.idiap.ch/
+#Written by Alireza Mohammadshahi <alireza.mohammadshahi@idiap.ch>,
+
+#This file is part of g2g-transformer.
+
+#g2g-transformer is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License version 2 as
+#published by the Free Software Foundation.
+
+#g2g-transformer is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with g2g-transformer. If not, see <http://www.gnu.org/licenses/>.
+
 from collections import namedtuple
 from typing import Dict, Tuple, List, Any, Callable
 from overrides import overrides
